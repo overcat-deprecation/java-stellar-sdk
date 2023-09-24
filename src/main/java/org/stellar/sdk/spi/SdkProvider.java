73041a3b5eb8fc88d0a0a1150d1b59914f69f62b
overcat <4catcode@gmail.com>
2023-09-24 16:48:37 +0800

    default implementation

--- src/main/java/org/stellar/sdk/spi/SdkProvider.java
+++ src/main/java/org/stellar/sdk/spi/SdkProvider.java
@@ -3,5 +3,8 @@ package org.stellar.sdk.spi;
 import org.stellar.sdk.Base64;
 
 public interface SdkProvider {
-  Base64 createBase64();
+  default Base64 createBase64() {
+    // we allow this to be null, if this is the case the SDK will use the default implementation
+    return null;
+  }
 }
